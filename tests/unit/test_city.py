175c61d58e08e25365a2f725ac8814233c409162
Vronst <vronst@gmail.com>
2025-11-17 21:31:21 +0100

    Fixed linting

--- tests/unit/test_city.py
+++ tests/unit/test_city.py
@@ -2,94 +2,157 @@
 from data_processor.city import City
 
 city1 = City(
-    'city1',
-    'big',
-    ['mine', 'port'],
+    "city1",
+    "big",
+    ["mine", "port"],
     100,
     1,
     {
-        "metal": {"quantity": 200, "price": 200, "regular_price": 150,
-            "regular_quantity": 100},
-        "gems": {"quantity": 100, "price": 250, "regular_price": 300,
-            "regular_quantity": 100},
-        "food": {"quantity": 150, "price": 60, "regular_price": 45,
-                 "regular_quantity": 100},
-        "fuel": {"quantity": 80, "price": 120, "regular_price": 80,
-                 "regular_quantity": 100},
-        "relics": {"quantity": 10, "price": 1300, "regular_price": 1300,
-                   "regular_quantity": 100},
-        "special": None
+        "metal": {
+            "quantity": 200,
+            "price": 200,
+            "regular_price": 150,
+            "regular_quantity": 100,
+        },
+        "gems": {
+            "quantity": 100,
+            "price": 250,
+            "regular_price": 300,
+            "regular_quantity": 100,
+        },
+        "food": {
+            "quantity": 150,
+            "price": 60,
+            "regular_price": 45,
+            "regular_quantity": 100,
+        },
+        "fuel": {
+            "quantity": 80,
+            "price": 120,
+            "regular_price": 80,
+            "regular_quantity": 100,
+        },
+        "relics": {
+            "quantity": 10,
+            "price": 1300,
+            "regular_price": 1300,
+            "regular_quantity": 100,
+        },
+        "special": None,
     },
     0,
     [],
-    ['city2']
+    ["city2"],
 )
 
 city2 = City(
-    'city2',
-    'small',
+    "city2",
+    "small",
     [],
     1,
     1,
     {
-        "metal": {"quantity": 20, "price": 20, "regular_price": 15,
-                  "regular_quantity": 10},
-        "gems": {"quantity": 10, "price": 20, "regular_price": 30,
-                 "regular_quantity": 10},
-        "food": {"quantity": 15, "price": 6, "regular_price": 4, 
-                 "regular_quantity": 10},
-        "fuel": {"quantity": 8, "price": 12, "regular_price": 8, 
-                 "regular_quantity": 10},
-        "relics": {"quantity": 1, "price": 100, "regular_price": 130, 
-                   "regular_quantity": 10},
-        "special": None
+        "metal": {
+            "quantity": 20,
+            "price": 20,
+            "regular_price": 15,
+            "regular_quantity": 10,
+        },
+        "gems": {
+            "quantity": 10,
+            "price": 20,
+            "regular_price": 30,
+            "regular_quantity": 10,
+        },
+        "food": {
+            "quantity": 15,
+            "price": 6,
+            "regular_price": 4,
+            "regular_quantity": 10,
+        },
+        "fuel": {
+            "quantity": 8,
+            "price": 12,
+            "regular_price": 8,
+            "regular_quantity": 10,
+        },
+        "relics": {
+            "quantity": 1,
+            "price": 100,
+            "regular_price": 130,
+            "regular_quantity": 10,
+        },
+        "special": None,
     },
     0,
     [],
-    ['city2']
+    ["city2"],
 )
 
 
 class TestCity:  # noqa
-    
     def test_compare(self):  # noqa
-        assert city1.compare(city2) == {'food': {'price_diff': -54,
-            'quantity_diff': -135,
-            'reg_price': 45,
-            'reg_quantity': 100},
-   'fuel': {'price_diff': -108,
-            'quantity_diff': -72,
-            'reg_price': 80,
-            'reg_quantity': 100},
-   'gems': {'price_diff': -230,
-            'quantity_diff': -90,
-            'reg_price': 300,
-            'reg_quantity': 100},
-   'metal': {'price_diff': -180,
-             'quantity_diff': -180,
-             'reg_price': 150,
-             'reg_quantity': 100},
-   'relics': {'price_diff': -1200,
-              'quantity_diff': -9,
-              'reg_price': 1300,
-              'reg_quantity': 100}}
-        assert city1.compare(city1) == {'food': {'price_diff': 0,
-            'quantity_diff': 0,
-            'reg_price': 45,
-            'reg_quantity': 100},
-   'fuel': {'price_diff': 0,
-            'quantity_diff': 0,
-            'reg_price': 80,
-            'reg_quantity': 100},
-   'gems': {'price_diff': 0,
-            'quantity_diff': 0,
-            'reg_price': 300,
-            'reg_quantity': 100},
-   'metal': {'price_diff': 0,
-             'quantity_diff': 0,
-             'reg_price': 150,
-             'reg_quantity': 100},
-   'relics': {'price_diff': 0,
-              'quantity_diff': 0,
-              'reg_price': 1300,
-              'reg_quantity': 100}}
+        assert city1.compare(city2) == {
+            "food": {
+                "price_diff": -54,
+                "quantity_diff": -135,
+                "reg_price": 45,
+                "reg_quantity": 100,
+            },
+            "fuel": {
+                "price_diff": -108,
+                "quantity_diff": -72,
+                "reg_price": 80,
+                "reg_quantity": 100,
+            },
+            "gems": {
+                "price_diff": -230,
+                "quantity_diff": -90,
+                "reg_price": 300,
+                "reg_quantity": 100,
+            },
+            "metal": {
+                "price_diff": -180,
+                "quantity_diff": -180,
+                "reg_price": 150,
+                "reg_quantity": 100,
+            },
+            "relics": {
+                "price_diff": -1200,
+                "quantity_diff": -9,
+                "reg_price": 1300,
+                "reg_quantity": 100,
+            },
+        }
+        assert city1.compare(city1) == {
+            "food": {
+                "price_diff": 0,
+                "quantity_diff": 0,
+                "reg_price": 45,
+                "reg_quantity": 100,
+            },
+            "fuel": {
+                "price_diff": 0,
+                "quantity_diff": 0,
+                "reg_price": 80,
+                "reg_quantity": 100,
+            },
+            "gems": {
+                "price_diff": 0,
+                "quantity_diff": 0,
+                "reg_price": 300,
+                "reg_quantity": 100,
+            },
+            "metal": {
+                "price_diff": 0,
+                "quantity_diff": 0,
+                "reg_price": 150,
+                "reg_quantity": 100,
+            },
+            "relics": {
+                "price_diff": 0,
+                "quantity_diff": 0,
+                "reg_price": 1300,
+                "reg_quantity": 100,
+            },
+        }
